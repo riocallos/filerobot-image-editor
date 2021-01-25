f054de3bb461983604a87bcfa7b5f68e58c37f16
Rio Marie A. Callos <riomariecallos@gmail.com>
2021-01-25 22:28:04 +0800

    updated header title

--- dist/assets/i18n/en.js
+++ dist/assets/i18n/en.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _default = {
-  'header.image_editor_title': ' ',
+  'header.image_editor_title': 'Image Editor',
   'header.toggle_fullscreen': 'Toggle fullscreen',
   'header.close': 'Close',
   'header.close_modal': 'Close window',
